4a958e5154ac4b5182a22dabfa8b3a3a8b4f360f
Ariel Feingold <ariel@seethru.healthcare>
2019-07-29 16:57:00 -0400

    react-router-dom setup

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Navigation from '../components/Navigation';
 
 
 const App = () => (
   <React.Fragment>
     <CssBaseline />
-    <h1>App</h1>
+      <Router>
+        <Navigation />
+      </Router>
   </React.Fragment>
 );
 
