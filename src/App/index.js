ac20a876da280488034ab6d8dec7ad1d209c148b
Ariel Feingold <ariel@seethru.healthcare>
2019-07-29 18:04:25 -0400

    navigation bar added

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import Navigation from '../components/Navigation';
+import NavigationBar from '../components/NavigationBar';
+
+let userAuth = true
+
+// useEffect(() => {
+//   // TODO: Update the userAuth from loaclStorage
+// });
 
 
 const App = () => (
   <React.Fragment>
     <CssBaseline />
       <Router>
-        <Navigation />
+        <NavigationBar isAuth={userAuth}/>
       </Router>
   </React.Fragment>
 );
